fdfe786c73c87850098c37627bfffe67ce107d14
ariafan <inspektor.po13@gmail.com>
2017-06-06 12:19:10 +0700

    Nethunterfy

--- nethunter.mk
+++ nethunter.mk
@@ -18,9 +18,9 @@
 $(call inherit-product, device/xiaomi/ido/full_ido.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/nethunter/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_ido
+PRODUCT_NAME := nethunter_ido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
